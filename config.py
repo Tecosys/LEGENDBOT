1da3df27a9daa9a6a621c609789a133e060a3ce5
ha4c3kr <121658562+ha4c3kr@users.noreply.github.com>
2023-01-05 19:53:11 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 
+    API_HASH = ""
+    # the name to display in your alive message
+    ALIVE_NAME = ""
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = ""
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
+    LEGEND_STRING = ""
+    # create a new bot in @botfather and fill the following vales with bottoken
+    BOT_TOKEN = ""
+    # command handler
+    HANDLER = "."
+    # command hanler for sudo
+    SUDO_HANDLER = "."
+    # External plugins repo
+    EXTRA_REPO = "https://github.com/ITS-LEGENDBOT/PLUGINS"
+    UPSTREAM_REPO = "pro"
+    # Your City's TimeZone
+    TZ = "Asia/Kolkata"
